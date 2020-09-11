0df9249fd7dee18fe445966c2949e30f6e1c3a50
goldguard822 <goldguard882@gmail.com>
2020-09-11 16:37:31 +0900

    51

--- js/touchSwipe_slide.js
+++ js/touchSwipe_slide.js
@@ -1225,7 +1225,6 @@ $(function(){
   });
 
   setTimeout(function(){
-    debugger;
     var _aa = $("div#div-igaw-ad-web_dhhy4kqh66dwxmf").find("iframe");
     _aa.css({width:"100%"});
   },3000);
