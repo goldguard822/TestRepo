cf616b197820e8c00a37444d984d16dea232da8b
goldguard822 <goldguard882@gmail.com>
2020-09-15 02:42:38 +0900

    56

--- js/test_php.js
+++ js/test_php.js
@@ -1,6 +1,7 @@
 $.ajax({
   type:"GET",
   url:"http://bmclub.synology.me/board/notice.php?filter=1&keyword=&mod=list&page=0",
+  dataType:"html",
   cache:false,
   success:function(data){
     debugger;
