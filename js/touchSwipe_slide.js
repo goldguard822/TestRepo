feb5bcdf8e7db6fae4b64047c392739506623684
goldguard822 <goldguard822@gmail.com>
2020-12-02 18:40:04 +0900

    20201202_13

--- js/touchSwipe_slide.js
+++ js/touchSwipe_slide.js
@@ -16,9 +16,9 @@ document.writeln('  <span role="slide_close" style="float:left; display:inline-b
 document.writeln('  <span role="slide_close" style="float:left; display:inline-block; width:100%; height:20px; background-color:transparent; touch-action:none;"></span>');
 document.writeln('  <div style="position: absolute; top: calc(50% + 10px); left: 50%; width:90%; transform: translate(-50%, calc(-50% + 10px)); background-color:rgba(255,255,255,0);">');
 if (_ad_type1 < 0) { //html 타입인경우
-  document.writeln('    <iframe src="'+ _ad_url1 +'" name="ad_iframe1" style="left: 0; position: relative; top: 0; border: 0; width: 100%; height:'+ _ad_height1 +'; touch-action:none; background-color:#fff;" allowtransparency="true" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>');
+  //document.writeln('    <iframe src="'+ _ad_url1 +'" name="ad_iframe1" style="left: 0; position: relative; top: 0; border: 0; width: 100%; height:'+ _ad_height1 +'; touch-action:none; background-color:#fff;" allowtransparency="true" marginwidth="0" marginheight="0" hspace="0" vspace="0" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>');
 } else { // js 타입인 경우
-  document.writeln('    <scri'+'pt type="text/javascript" src="'+ _ad_url1 +'"></scri'+'pt>'); // 이 부분에 광고 스크립트 들어가야 함 **** src에 광고 url 변경 필요 ****
+  //document.writeln('    <scri'+'pt type="text/javascript" src="'+ _ad_url1 +'"></scri'+'pt>'); // 이 부분에 광고 스크립트 들어가야 함 **** src에 광고 url 변경 필요 ****
 }
 document.writeln('  </div>');
 document.writeln('</div>');
@@ -29,8 +29,9 @@ var _ad_url2 = "//tm.interworksmedia.co.kr/ads.js/4B006C06";
 //var _ad_url2 = "//adv.imadrep.co.kr/7826_02.html";
 
 document.writeln('<div name="ad_bottom1" style="position: fixed; bottom: 0;width: 100%;">');
-//document.writeln('  <scri'+'pt type="text/javascript" src="'+ _ad_url2 +'"></scri'+'pt>'); // 이 부분에 광고 스크립트 들어가야 함 **** src에 광고 url 변경 필요 ****
+document.writeln('  <scri'+'pt type="text/javascript" src="'+ _ad_url2 +'"></scri'+'pt>'); // 이 부분에 광고 스크립트 들어가야 함 **** src에 광고 url 변경 필요 ****
 document.writeln('</div>');
+/*
 setTimeout(function(){
   var _ad_bottom1_area = $("div[name='ad_bottom1']")[0];
   var _ad_bottom1_script = document.createElement("script");
@@ -38,6 +39,7 @@ setTimeout(function(){
   _ad_bottom1_script.type = "text/javascript";
   _ad_bottom1_area.appendChild(_ad_bottom1_script);
 },3000);
+*/
 /* 하단 광고 E */
 
 (function (factory) {
